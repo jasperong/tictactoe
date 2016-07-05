99b347737efa1972ec2f736cc5639a955a79b60d
Jasper <jave.ong4@yahoo.com>
2016-07-05 18:32:30 -0400

    Made click function

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 $(document).on('ready', function(){
+// define variables
+
+// if player clicks box, either x for turn odd or o for turn even
+var turn = 0;
+$('.box').on('click', function(){
+  if (turn % 2 == 0 && $(this).text() == " ") {
+    $(this).text("X");
+} else if (turn % 2 != 0 && $(this).text() == " "){
+    $(this).text("O");
+} turn++;
+  });
+
+// if 3 boxes match x/o, player wins
+
+// if no one matches 3, draw
 
 });
